1df50ce3343e0b54e07aea91f9d52294b47037af
Galuh Apriliano <galuhapriliano30@gmail.com>
2021-03-23 09:06:47 +0700

    Add files via upload

---
+++ MINGGU 3/Arraynama.java
@@ -0,0 +1,9 @@
+public class Arraynama {
+    public static void main(String args []){
+        String [] nama ={"Galuh", "Apriliano"};
+        for (int i=0; i<nama.length; i++){
+            System.out.print(nama [i]);
+            System.out.print(" ");
+        }
+    }
+}
