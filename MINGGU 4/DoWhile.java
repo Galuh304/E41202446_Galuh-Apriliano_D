6d8332b543bb2d8010837889f889988a9d8dbf82
Galuh Apriliano <galuhapriliano30@gmail.com>
2021-03-29 22:31:53 +0700

    Add files via upload

---
+++ MINGGU 4/DoWhile.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class DoWhile
+{
+    Scanner input=new Scanner(System.in);
+    public void counter()
+    {
+        int hitung=1;
+        do
+        {
+            System.out.print("  "+hitung);
+            hitung=(hitung*2);
+        }
+        while(hitung<=100);
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("         Do__While         ");
+        System.out.println("Bilangan kelipatan 2 (1-100)");
+        System.out.println("============================");
+        DoWhile mulai=new DoWhile();
+        mulai.counter();
+        System.out.println();
+    }
+
+}
